fe9074f325845cd33f71635221a538fd1c715e43
Debra Lemak <debra.lemak@gmail.com>
2017-11-06 15:47:50 -0600

    Using counter to taper off after button release

--- squishPond/squishPond.ino
+++ squishPond/squishPond.ino
@@ -1,99 +1,80 @@
 //Squish Pond for East Austin Studio Tour 2017 - Debra Lemak 
-// Using "Ripple" Conversion by Andrew Tuline
-//
+
+#define NUM_LEDS_PER_STRIP 5
+#define LED_TYPE   WS2811
+#define COLOR_ORDER   RGB
+
+#define STRAND11        11
+#define STRAND12        12
+#define STRAND13        13
 
 
-#include <FastLED.h>        // FastLED library
-
- 
-#define LED_DT 11
-#define NUM_LEDS 24
-
-struct CRGB leds[NUM_LEDS];
- 
-int color;
-int center = 0;
-int step = -1;
-int maxSteps = 16;
-float fadeRate = 0.8;
-int diff;
 int buttonPin = 3; 
 int buttonState = 0;
-int hueValue = 140;
-int splashZone01 [] = {0,1,2,3,4,5,6
-}; 
-//background color
-uint32_t currentBg = hueValue;
-uint32_t nextBg = currentBg;
- 
-void setup() {
+int offCounter = 0 
 
-  Serial.begin(9600);
-  LEDS.addLeds<WS2811, LED_DT, GRB>(leds, NUM_LEDS);
+
+CRGB leds[NUM_LEDS_PER_STRIP];
+
+void setup() {
+  delay( 3000 ); //safety startup delay
+  FastLED.addLeds<LED_TYPE,STRAND11,COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP)
+  FastLED.addLeds<LED_TYPE,STRAND12,COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP)
+  FastLED.addLeds<LED_TYPE,STRAND13,COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP)
+  ///Using FastLED reference to mirror strips - https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples 
+  .setCorrection(TypicalLEDStrip);
   pinMode(buttonPin, INPUT); 
 }
- 
-void loop () {
-  buttonState = digitalRead(buttonPin);
- if (buttonState ==HIGH) {
-  ripple();
-  }
-}
- 
-void ripple() {
 
-    if (currentBg == nextBg) {
-      nextBg = hueValue;
-    } 
-    else if (nextBg > currentBg) {
-      currentBg++;
-    } else {
-      currentBg--;
-    }
-    for(uint16_t l = 0; l < NUM_LEDS; l++) {
-      leds[l] = CHSV(currentBg, 255, 50);         // strip.setPixelColor(l, Wheel(currentBg, 0.1));
-    }
- 
-  if (step == -1) {
-    center = random(NUM_LEDS);
-    color = hueValue;
-    step = 0;
-  }
- 
-  if (step == 0) {
-    leds[center] = CHSV(color, 255, 255);         
-    step ++;
-  } 
-  else {
-    if (step < maxSteps) {
-      Serial.println(pow(fadeRate,step));
 
-      leds[wrap(center + step)] = CHSV(color, 255, pow(fadeRate, step)*255);       
-      leds[wrap(center - step)] = CHSV(color, 255, pow(fadeRate, step)*255);       
-      if (step > 3) {
-        leds[wrap(center + step - 3)] = CHSV(color, 255, pow(fadeRate, step - 2)*255);     
-        leds[wrap(center - step + 3)] = CHSV(color, 255, pow(fadeRate, step - 2)*255);    
-      }
-      step ++;
-    } 
-    else {
-      step = -1;
-    }
-  }
+void loop() {
+  ///referencing color options from: https://github.com/FastLED/FastLED/wiki/Pixel-reference
+
   
-  LEDS.show();
-  delay(50);
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState ==HIGH) {
+    leds[0] = CRGB::Turquoise;
+    leds[1] = CRGB::Turquoise;
+    leds[2] = CRGB::AliceBlue;
+    leds[3] = CRGB::AliceBlue;
+    leds[4] = CRGB::AliceBlue;
+    addGlitter(80);
+    offCounter = 0; 
+     }
+   else if (buttonState == LOW) { 
+   offCounter++; 
+   if offCounter < 300) {
+   //referencing fading from https://github.com/FastLED/FastLED/wiki/Pixel-reference 
+     leds[0].fadeLightBy( 90);
+     leds[0].fadeLightBy( 90);
+     leds[0].fadeLightBy( 64);
+     leds[0].fadeLightBy( 64);
+     leds[0].fadeLightBy( 64);
+     addGlitter(30); 
+     }
+   else if offCounter < 1000) {
+     leds[0].fadeLightBy( 200);
+     leds[0].fadeLightBy( 200);
+     leds[0].fadeLightBy( 90);
+     leds[0].fadeLightBy( 90);
+     leds[0].fadeLightBy( 90);
+     addGlitter(5); 
+     }
+   }
+   else  {
+   fill_solid( leds, NUM_LEDS, Seashell);
+   }
+   FastLED.show();
 }
- 
- 
-int wrap(int step) {
-  if(step < 0) return NUM_LEDS + step;
-  if(step > NUM_LEDS - 1) return step - NUM_LEDS;
-  return step;
+
+
+
+//////
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS_PER_STRIP) ] += CRGB::White;
+  }
 }
- 
-
-
-
- 
-
+////////////
