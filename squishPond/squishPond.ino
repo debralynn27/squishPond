fdf14a678a76e4ac6825bbcb9a8724f155be1ad5
Debra Lemak <debra.lemak@gmail.com>
2017-11-18 14:39:57 -0600

    updated intervals

--- squishPond/squishPond.ino
+++ squishPond/squishPond.ino
@@ -7,7 +7,7 @@
 #define NUM_STRIPS 3
 #define NUM_LEDS NUM_LEDS_PER_STRIP*NUM_STRIPS
 #define LED_TYPE   WS2811
-#define COLOR_ORDER   RGB
+#define COLOR_ORDER   RGB 
 #define BRIGHTNESS  80
 #define FRAMES_PER_SECOND 60
 #define SENSOR1 7
@@ -19,7 +19,9 @@ int sensorPressed = 0;
 int sensor1Counter = 0;
 unsigned long startTime = millis();
 unsigned long firstInterval = 15000UL; // 15 seconds
-unsigned long secondInterval = 30000UL; // 15 seconds
+unsigned long secondInterval = 25000UL; // 10 seconds
+unsigned long thirdInterval = 40000UL; // 15 seconds
+unsigned long fourthInterval = 60000UL; // 20 seconds
 
 CRGB cluster1[NUM_LEDS_PER_STRIP];
 
@@ -40,29 +42,16 @@ void setup() {
 //////////////////////////////////////////////////////////////////////////////////
 
 void loop() {
-  /*
-  if (digitalRead(sensorPin01) == HIGH) {
-       Serial.println ("sensor1 pressed");
-       Serial.print ("  ");
-       sensorPressed = 1;
-       aquapaint();
-       addGlitter(80);
-       sensor1Counter = 1;
-  } else {
-      Serial.print ("no sensor1");
-      FastLED.clear();
-      sensor1trail();
-      sensorPressed = 0;
-  }  
-  */
-
   if(millis() - startTime < firstInterval)
   {
     aquapaint();
     addGlitter(80);
   } else if (millis() - startTime < secondInterval) {
-    FastLED.clear();
-    sensor1trail();
+    trailoff1();
+  } else if (millis() - startTime < thirdInterval) {
+    trailoff2(); 
+  } else if (millis() - startTime < fourthInterval) {
+    FastLED.clear();  
   } else {
     startTime = millis();
   }
@@ -86,10 +75,7 @@ void addGlitter( fract8 chanceOfGlitter)
   }
 }
 
-void sensor1trail() {
-  if (sensorPressed != 1 && sensor1Counter>=1) { 
-   sensor1Counter++; 
-   if (sensor1Counter < 300) {
+void trailoff1() {
    //referencing fading from /https://github.com/FastLED/FastLED/wiki/Pixel-reference 
      cluster1[0].fadeLightBy( 90);
      cluster1[1].fadeLightBy( 90);
@@ -98,7 +84,8 @@ void sensor1trail() {
      cluster1[4].fadeLightBy( 64);
      addGlitter(30); 
      }
-   if (sensor1Counter < 1000) {
+
+void trailoff2() {
      cluster1[0].fadeLightBy( 200);
      cluster1[1].fadeLightBy( 200);
      cluster1[2].fadeLightBy( 90);
@@ -106,128 +93,4 @@ void sensor1trail() {
      cluster1[4].fadeLightBy( 90);
      addGlitter(5); 
      }
-    else if (sensor1Counter > 2000){
-      sensor1Counter = 0; 
-    }
-   }
-}
-
-/*
-  
-  
-  } 
-if (sensor2 >= 1000) {
-    sensorPressed = s2on;
-    return sensorPressed;
-  } 
-    if (sensor3 >= 1000) {
-    sensorPressed = s3on;
-    return sensorPressed;
-  } 
-
-  sensorPressed = 0;
-}
-
-void setActive() {
-   switch (sensorPressed) {
-    case s1on:
-       cluster1[0] = CRGB::Turquoise;
-       cluster1[1] = CRGB::Turquoise;
-       cluster1[2] = CRGB::AliceBlue;
-       cluster1[3] = CRGB::AliceBlue;
-       cluster1[4] = CRGB::AliceBlue;
-       addGlitter1(80);
-       sensor1Counter = 1;
-      break;
-      
-  /*  case s2on:
-       cluster2[0] = CRGB::Turquoise;
-       cluster2[1] = CRGB::Turquoise;
-       cluster2[2] = CRGB::AliceBlue;
-       cluster2[3] = CRGB::AliceBlue;
-       cluster2[4] = CRGB::AliceBlue;
-       addGlitter2(80);
-       sensor2Counter = 1;
-      break;
-      
-    case s3on:
-       cluster3[0] = CRGB::Turquoise;
-       cluster3[1] = CRGB::Turquoise;
-       cluster3[2] = CRGB::AliceBlue;
-       cluster3[3] = CRGB::AliceBlue;
-       cluster3[4] = CRGB::AliceBlue;
-       addGlitter3(80);
-       sensor3Counter = 1;
-      break;
-}
-}
-
-
-
-void sensor2trail() {
-  if (sensorPressed != s2on && sensor2Counter>=1) { 
-   sensor2Counter++; 
-   if (sensor2Counter < 300) {
-     cluster2[0].fadeLightBy( 90);
-     cluster2[1].fadeLightBy( 90);
-     cluster2[2].fadeLightBy( 64);
-     cluster2[3].fadeLightBy( 64);
-     cluster2[4].fadeLightBy( 64);
-     addGlitter2(30); 
-     }
-   if (sensor2Counter < 1000) {
-     cluster2[0].fadeLightBy( 200);
-     cluster2[1].fadeLightBy( 200);
-     cluster2[2].fadeLightBy( 90);
-     cluster2[3].fadeLightBy( 90);
-     cluster2[4].fadeLightBy( 90);
-     addGlitter2(5); 
-     }
-    else if (sensor2Counter > 2000){
-      sensor2Counter = 0; 
-    }
-   }
-   else  {
-   fill_solid( cluster2, NUM_LEDS, CRGB::Seashell);
-   }
-}
-
-void sensor3trail() {
-  if (sensorPressed != s3on && sensor3Counter>=1) { 
-   sensor3Counter++; 
-   if (sensor3Counter < 300) {
-     cluster3[0].fadeLightBy( 90);
-     cluster3[1].fadeLightBy( 90);
-     cluster3[2].fadeLightBy( 64);
-     cluster3[3].fadeLightBy( 64);
-     cluster3[4].fadeLightBy( 64);
-     addGlitter3(30); 
-     }
-   if (sensor3Counter < 1000) {
-     cluster3[0].fadeLightBy( 200);
-     cluster3[1].fadeLightBy( 200);
-     cluster3[2].fadeLightBy( 90);
-     cluster3[3].fadeLightBy( 90);
-     cluster3[4].fadeLightBy( 90);
-     addGlitter3(5); 
-     }
-    else if (sensor3Counter > 2000){
-      sensor3Counter = 0; 
-    }
-   }
-   else  {
-   fill_solid( cluster3, NUM_LEDS, CRGB::Seashell);
-   }
-}
-
-void setTrailing(){
-    sensor1trail();
-  //  sensor2trail();
-   // sensor3trail();
-    }
-
-*/
-
-
-  
 
